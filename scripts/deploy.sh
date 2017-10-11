5949afcc9b3f83cb07fc12e6a321687e367dade8
Diego Hernandez <Diego.Hernandez@ibm.com>
2017-10-11 15:04:28 -0500

    ci(Travis): Remove current deploy commands to check if travis is working

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-webstore upload --auto-publish --source ./src --extension-id $CHROME_EXTENSION_ID --client-id $CHROME_CLIENT_ID --client-secret $CHROME_CLIENT_SECRET --refresh-token $CHROME_REFRESH_TOKEN
-web-ext build -o -s ./src -a ./dist; node ./scripts/firefox_deploy.js
+# webstore upload --auto-publish --source ./src --extension-id $CHROME_EXTENSION_ID --client-id $CHROME_CLIENT_ID --client-secret $CHROME_CLIENT_SECRET --refresh-token $CHROME_REFRESH_TOKEN
+# web-ext build -o -s ./src -a ./dist; node ./scripts/firefox_deploy.js
+
+echo '********************* DEPLOY GOES HERE ***************************'
